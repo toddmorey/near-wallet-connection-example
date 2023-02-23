e71758cd114b9051d944a15bdc3162e51f0e7318
Alexander Fadeev <fadeevab.com@gmail.com>
2023-02-23 21:34:25 +0200

    Get the signer with a selected wallet and near connection

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ import { setupNearWallet } from '@near-wallet-selector/near-wallet';
 import { setupWalletConnect } from '@near-wallet-selector/wallet-connect';
 import { Buffer } from 'buffer';
 import '@near-wallet-selector/modal-ui/styles.css';
+import * as nearAPI from 'near-api-js';
 
 // buffer pollyfill needed by Near Wallet Selector
 globalThis.Buffer = Buffer;
@@ -18,3 +19,21 @@ const modal = setupModal(selector, {
 });
 
 modal.show();
+
+const selectorWallet = await selector.wallet();
+const selectorAccount = (await selectorWallet.getAccounts()).shift(); // Get the 1st account
+
+const config = {
+  networkId: 'testnet',
+  keyStore: new nearAPI.keyStores.BrowserLocalStorageKeyStore(),
+  nodeUrl: 'https://rpc.testnet.near.org',
+};
+
+const near = await nearAPI.connect(config);
+const signer = near.connection.signer;
+const signerPublicKey = (
+  await signer.getPublicKey(selectorAccount.accountId, config.networkId)
+).toString();
+
+console.log(selectorAccount);
+console.log(signerPublicKey);
